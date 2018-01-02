fca9c6eb8f0291485f03d0a889fb8d6a3df199c1
TreeMaster <nutech@outlook.com>
2018-01-02 19:56:11 +0700

    Update LineApi.py

--- LINETCR/LineApi.py
+++ LINETCR/LineApi.py
@@ -311,7 +311,7 @@ class LINE:
 
       prof = self.getProfile()
 
-      print("Yoshino Bot")
+      print("Tree Bot")
       print("mid -> " + prof.mid)
       print("name -> " + prof.displayName)
       print("authToken -> " + self.authToken)
